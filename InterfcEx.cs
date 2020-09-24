3b6437cc7017f7e0cd1c1d07652db70269e3d1ca
AmorFatiBA <you@example.com>
2020-09-24 14:28:30 +0300

    first commit

---
+++ InterfcEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Calisma37_InterfcEx
+{
+    class Koleksiyon : IEnumerable<int>
+    {
+        public int[] Dizi;
+
+        public Koleksiyon(int[] dizi)
+        {
+            Dizi = dizi;
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            for (int i = 0; i < Dizi.Length; i++)
+            {
+                // if (Dizi[i]>50)
+                // yield break;
+                yield return Dizi[i];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Calisma37_InterfcEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            int[] dizi = new int[11] {55, 3, 6, 5, 9, 4, 8, 12, 69, 48, 0 };
+
+            Koleksiyon k = new Koleksiyon(dizi);
+
+            ArrayList list = new ArrayList();
+
+
+
+            foreach (var sayi in k)
+            {
+                list.Add((int)sayi);
+            }   
+
+            list.Sort();
+
+            foreach (object i in list)
+                Console.Write((int)i + " ");
+
+            Console.WriteLine();
+        }
+    }
+}
